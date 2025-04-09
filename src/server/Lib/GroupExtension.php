bf1c1e451bbfddcc561a9b048b86abdd404dc53a
John Kraczek <john@kraczek.com>
2025-04-09 12:39:03 -0600

    Allowed the end user to choose display type from Shortcode, Redirect, Saved Section

--- src/server/Lib/GroupExtension.php
+++ src/server/Lib/GroupExtension.php
@@ -27,21 +27,31 @@ class GroupExtension extends \BP_Group_Extension
     public function display($group_id = null)
     {
         $group_id = bp_get_group_id();
-        $group_extension_shortcode = groups_get_groupmeta($group_id, 'group_extension_shortcode');
-        $group_tab_name = groups_get_groupmeta($group_id, 'group_tab_name');
+        $display_type = groups_get_groupmeta($group_id, 'group_display_type');
+        $shortcode = groups_get_groupmeta($group_id, 'group_extension_shortcode');
+        $elementor_shortcode = groups_get_groupmeta($group_id, 'group_extension_elementor');
+        $redirect_url = groups_get_groupmeta($group_id, 'group_redirect_url');
 
-        echo '<div class="bp-widget groups.group-admin">';
-        if (!empty($group_extension_shortcode)) {
-            echo do_shortcode($group_extension_shortcode);
+        echo '<div class="bp-widget">';
+        if ($display_type === 'shortcode' && !empty($shortcode)) {
+            echo do_shortcode($shortcode);
+        } elseif ($display_type === 'redirect' && !empty($redirect_url)) {
+            echo 'Do A Redirect Here';
+            echo $redirect_url;
+            echo '<script>window.location.href = "' . esc_url($redirect_url) . '";</script>';
+        } elseif ($display_type === 'elementor' && !empty($elementor_shortcode)) {
+            echo do_shortcode($elementor_shortcode);
         } else {
             echo '<p>' . __('No content has been set for this', 'ydtb-group-tabs') . '</p>';
         }
         echo '</div>';
     }
-
     public function settings_screen($group_id = null)
     {
+        $display_type = groups_get_groupmeta($group_id, 'group_display_type');
         $shortcode = groups_get_groupmeta($group_id, 'group_extension_shortcode');
+        $elementor = groups_get_groupmeta($group_id, 'group_extension_elementor');
+        $redirect_url = groups_get_groupmeta($group_id, 'group_redirect_url');
         $group_tab_name = groups_get_groupmeta($group_id, 'group_tab_name');
         $group_tab_visibility = groups_get_groupmeta($group_id, 'group_tab_visibility');
         $saved_sections = $this->get_saved_sections(); // Fetch saved Elementor sections
@@ -52,16 +62,62 @@ class GroupExtension extends \BP_Group_Extension
         <input type="text" name="group_tab_name" id="group_tab_name" value="<?php echo esc_attr($group_tab_name); ?>"
             style="width: 100%;" placeholder="<?php _e('Enter tab name', 'ydtb-group-tabs'); ?>">
         <br>
-        <p><?php _e('Select an Elementor Pro saved section to display content on this tab.', 'ydtb-group-tabs'); ?></p>
-        <select name="group_extension_shortcode" id="group_extension_shortcode" style="width: 100%;">
-            <option value=""><?php _e('Select a section', 'ydtb-group-tabs'); ?></option>
-            <?php foreach ($saved_sections as $section_id => $section_title): ?>
-                <option value="[elementor-template id=&quot;<?php echo esc_attr($section_id); ?>&quot;]" <?php selected($shortcode, '[elementor-template id="' . $section_id . '"]'); ?>>
-                    <?php echo esc_html($section_title); ?>
-                </option>
-            <?php endforeach; ?>
+        <p><?php _e('Select what happens when the tab is clicked.', 'ydtb-group-tabs'); ?></p>
+        <select name="group_display_type" id="group_display_type" style="width: 100%;">
+            <option value="shortcode" <?php selected($display_type, 'shortcode'); ?>>
+                <?php _e('Display Custom Shortcode', 'ydtb-group-tabs'); ?>
+            </option>
+            <option value="redirect" <?php selected($display_type, 'redirect'); ?>>
+                <?php _e('Redirect User To URL', 'ydtb-group-tabs'); ?>
+            </option>
+            <option value="elementor" <?php selected($display_type, 'elementor'); ?>>
+                <?php _e('Display Elementor Pro Saved Section', 'ydtb-group-tabs'); ?>
+            </option>
         </select>
         <br><br>
+
+        <div id="shortcode_field" style="display: <?php echo $display_type === 'shortcode' ? 'block' : 'none'; ?>;">
+            <p><?php _e('Enter the custom shortcode to display.', 'ydtb-group-tabs'); ?></p>
+            <input type="text" name="group_extension_shortcode" id="group_extension_shortcode"
+                value="<?php echo esc_attr($shortcode); ?>" style="width: 100%;"
+                placeholder="<?php _e('Enter shortcode', 'ydtb-group-tabs'); ?>">
+        </div>
+
+        <div id="redirect_field" style="display: <?php echo $display_type === 'redirect' ? 'block' : 'none'; ?>;">
+            <p><?php _e('Enter the URL to redirect to.', 'ydtb-group-tabs'); ?></p>
+            <input type="text" name="group_redirect_url" id="group_redirect_url" value="<?php echo esc_attr($redirect_url); ?>"
+                style="width: 100%;" placeholder="<?php _e('Enter URL', 'ydtb-group-tabs'); ?>">
+        </div>
+
+        <div id="elementor_field" style="display: <?php echo $display_type === 'elementor' ? 'block' : 'none'; ?>;">
+            <p><?php _e('Select an Elementor Pro saved section to display content on this tab.', 'ydtb-group-tabs'); ?></p>
+            <select name="group_extension_elementor" id="group_extension_elementor" style="width: 100%;">
+                <option value=""><?php _e('Select a section', 'ydtb-group-tabs'); ?></option>
+                <?php foreach ($saved_sections as $section_id => $section_title): ?>
+                    <option value="[elementor-template id=&quot;<?php echo esc_attr($section_id); ?>&quot;]" <?php selected($elementor, '[elementor-template id="' . $section_id . '"]'); ?>>
+                        <?php echo esc_html($section_title); ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                const displayType = document.getElementById('group_display_type').value;
+                document.getElementById('shortcode_field').style.display = displayType === 'shortcode' ? 'block' : 'none';
+                document.getElementById('redirect_field').style.display = displayType === 'redirect' ? 'block' : 'none';
+                document.getElementById('elementor_field').style.display = displayType === 'elementor' ? 'block' : 'none';
+            });
+
+            document.getElementById('group_display_type').addEventListener('change', function () {
+                const displayType = this.value;
+                document.getElementById('shortcode_field').style.display = displayType === 'shortcode' ? 'block' : 'none';
+                document.getElementById('redirect_field').style.display = displayType === 'redirect' ? 'block' : 'none';
+                document.getElementById('elementor_field').style.display = displayType === 'elementor' ? 'block' : 'none';
+            });
+        </script>
+
+        <br>
         <p><?php _e('Set who can see this tab.', 'ydtb-group-tabs'); ?></p>
         <?php
         $visibility_options = array(
@@ -86,13 +142,19 @@ class GroupExtension extends \BP_Group_Extension
 
     public function settings_screen_save($group_id = null)
     {
+        $display_type = isset($_POST['group_display_type']) ? sanitize_text_field($_POST['group_display_type']) : 'shortcode';
         $shortcode = isset($_POST['group_extension_shortcode']) ? sanitize_text_field($_POST['group_extension_shortcode']) : '';
+        $elementor_shortcode = isset($_POST['group_extension_elementor']) ? sanitize_text_field($_POST['group_extension_elementor']) : '';
+        $redirect_url = isset($_POST['group_redirect_url']) ? esc_url_raw($_POST['group_redirect_url']) : '';
         $group_tab_name = isset($_POST['group_tab_name']) ? sanitize_text_field($_POST['group_tab_name']) : '';
         $group_tab_visibility = isset($_POST['group_tab_visibility']) ? sanitize_text_field($_POST['group_tab_visibility']) : 'anyone';
 
+        groups_update_groupmeta($group_id, 'group_display_type', $display_type);
         groups_update_groupmeta($group_id, 'group_extension_shortcode', $shortcode);
+        groups_update_groupmeta($group_id, 'group_redirect_url', $redirect_url);
         groups_update_groupmeta($group_id, 'group_tab_name', $group_tab_name);
         groups_update_groupmeta($group_id, 'group_tab_visibility', $group_tab_visibility);
+        groups_update_groupmeta($group_id, 'group_extension_elementor', $elementor_shortcode);
     }
 
     private function get_saved_sections()
