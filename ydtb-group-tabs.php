5fc70ecf95c7f388cea790d018caec779debde17
John Kraczek <john@kraczek.com>
2025-04-10 00:47:09 +0000

    Fix Workflow

--- ydtb-group-tabs.php
+++ ydtb-group-tabs.php
@@ -6,7 +6,7 @@
  * Author: Your Digital Toolbox
  * Author URI: https://yourdigitaltoolbox.com
  * Description: A simple plugin to create tab groups in WordPress.
- * Version: 0.0.5 
+ * Version: 0.0.6 
  * Text Domain: ydtb-group-tabs
  * Domain Path: /languages  
  * License: GPL2
